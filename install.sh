ee5b1f76560430be9c0447017f48bebddd728124
Andrew Hayzen <ahayzen@gmail.com>
2022-12-29 01:15:54 +0000

    devbox: rearrange folders into data dn src

--- install.sh
+++ install.sh
@@ -14,13 +14,9 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 # Symlink the binary
 mkdir -p "$HOME/.local/bin"
 if [ ! -x "$HOME/.local/bin/devbox" ]; then
-    ln -s "$SCRIPTPATH/bin/devbox.sh" "$HOME/.local/bin/devbox"
+    ln -sf "$SCRIPTPATH/src/devbox.sh" "$HOME/.local/bin/devbox"
 fi
 
-# Install common parts
-mkdir -p "$DATA_FOLDER/common"
-cp -R "$SCRIPTPATH/common" "$DATA_FOLDER/common"
-
-# Install example container
-mkdir -p "$DATA_FOLDER/containers"
-cp -R "$SCRIPTPATH/containers" "$DATA_FOLDER/containers"
+# Install data
+mkdir -p "$DATA_FOLDER"
+cp -R "$SCRIPTPATH/data/" "$DATA_FOLDER/"
