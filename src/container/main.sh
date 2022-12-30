06a2a69ae226ef2e73914e543298d29fd2f8d6c1
Andrew Hayzen <ahayzen@gmail.com>
2022-12-30 13:01:42 +0000

    devbox: fix container printing to clear lines

--- src/container/main.sh
+++ src/container/main.sh
@@ -3,22 +3,22 @@
 # SPDX-License-Identifier: MPL-2.0
 
 function container_setup() {
-    printf "(%s) Finding Podman ... " "$TAG_NAME"
+    printf "\r(%s) Finding Podman ... " "$TAG_NAME"
     container_find_podman
 
     # Build the container image
-    printf "(%s) Building ... " "$TAG_NAME"
+    printf "\r(%s) Building image ... " "$TAG_NAME"
     container_setup_build
-    printf "\r\033[0K"
 
     # Create the container with the args, this could take a while the first time with userns=keep-id
-    printf "(%s) Creating (first time may take a while) ... " "$TAG_NAME"
+    printf "\r(%s) Creating container (first time may take a while) ... " "$TAG_NAME"
     container_setup_create
-    printf "\r\033[0K"
 
     # Start the container
-    printf "(%s) Starting ... " "$TAG_NAME"
+    printf "\r(%s) Starting container ... " "$TAG_NAME"
     container_setup_start
+
+    # Clear console
     printf "\r\033[0K"
 
     # Attach to the container
