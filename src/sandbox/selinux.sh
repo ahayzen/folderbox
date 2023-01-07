7384bd420f0e56fb4570625e6b3a16745506b5e5
Andrew Hayzen <ahayzen@gmail.com>
2023-01-07 15:41:39 +0000

    selinux: disable for now instead of container_runtime_t
    This allows for vscode code-server to be launched via podman exec.
    
    Closes #6

--- src/sandbox/selinux.sh
+++ src/sandbox/selinux.sh
@@ -3,5 +3,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 function sandbox_setup_selinux() {
-    CONTAINER_RUN_ARGS+=(--security-opt=label=type:container_runtime_t)
+    # For now we disable SELinux within the container
+    #
+    # This could be label=type:container_runtime_t but then vscode code-server
+    # fails to start via podman exec CONTAINER ~/.vscode-server/...
+    #
+    # It does however work inside bash with bash -c ~/.vscode-server/...
+    # https://github.com/ahayzen/folderbox/issues/6
+    #
+    # As there are likely other cases which may cause issues lets disable for now.
+    CONTAINER_RUN_ARGS+=(--security-opt=label=disable)
 }
