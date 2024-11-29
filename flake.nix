ee8c08e0dc24a035a09e2cd3ddde91c208b3a5a4
Andrew Hayzen <ahayzen@gmail.com>
2024-11-29 22:00:52 +0000

    flake: add a flake for using folderbox

---
+++ flake.nix
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux = {
+      default = pkgs.callPackage ./package.nix {};
+    };
+  };
+}
---
+++ package.nix
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{ stdenv
+, lib
+, fetchFromGitHub
+, bash
+, makeWrapper
+}:
+  stdenv.mkDerivation {
+    pname = "folderbox";
+    version = "0.2.0";
+    src = lib.cleanSource ./.;
+    buildInputs = [ bash ];
+    buildPhase = ''
+      bash ./build.sh
+    '';
+    nativeBuildInputs = [ makeWrapper ];
+    installPhase = ''
+      mkdir -p $out/bin
+      cp ./bin/folderbox $out/bin/folderbox
+      wrapProgram $out/bin/folderbox \
+        --prefix PATH : ${lib.makeBinPath [ bash ]}
+
+      mkdir -p $out/share/templates/
+      cp ./data/common/* $out/share/templates/
+    '';
+  }
+
