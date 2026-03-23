586f54007299dc471b453d9562830b81b2b20da9
Andrew Hayzen <ahayzen@gmail.com>
2026-03-23 20:42:04 +0000

    opencode: add to sandbox and common

---
+++ src/sandbox/opencode.sh
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+function sandbox_setup_opencode() {
+    # If there is an opencode config then mount it into the container
+    if [ -f "$HOME/.config/opencode/opencode.json" ]; then
+        # Ensure parent of mounted opencode folder exists
+        # otherwise .config is root in the container
+        mkdir -p "$PERSIST_FOLDER/home/.config/opencode"
+
+        CONTAINER_RUN_ARGS+=(--volume="$HOME/.config/opencode/opencode.json":"$HOME/.config/opencode/opencode.json":ro)
+    fi
+
+    # If there is an opencode tui config then mount it into the container
+    if [ -f "$HOME/.config/opencode/tui.json" ]; then
+        # Ensure parent of mounted opencode folder exists
+        # otherwise .config is root in the container
+        mkdir -p "$PERSIST_FOLDER/home/.config/opencode"
+
+        CONTAINER_RUN_ARGS+=(--volume="$HOME/.config/opencode/tui.json":"$HOME/.config/opencode/tui.json":ro)
+    fi
+}
+
