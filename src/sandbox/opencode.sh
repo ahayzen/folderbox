d28988dcfc11589d9d7803ef035d8e40c9ea6e27
Andrew Hayzen <ahayzen@gmail.com>
2026-03-24 22:47:46 +0000

    sandbox: add config.json to opencode passthroughs

--- src/sandbox/opencode.sh
+++ src/sandbox/opencode.sh
@@ -4,6 +4,17 @@
 
 function sandbox_setup_opencode() {
     # If there is an opencode config then mount it into the container
+    #
+    # NOTE: older versions of opencode use config.json
+    if [ -f "$HOME/.config/opencode/config.json" ]; then
+        # Ensure parent of mounted opencode folder exists
+        # otherwise .config is root in the container
+        mkdir -p "$PERSIST_FOLDER/home/.config/opencode"
+
+        CONTAINER_RUN_ARGS+=(--volume="$HOME/.config/opencode/config.json":"$HOME/.config/opencode/config.json":ro)
+    fi
+
+    # If there is an opencode config then mount it into the container
     if [ -f "$HOME/.config/opencode/opencode.json" ]; then
         # Ensure parent of mounted opencode folder exists
         # otherwise .config is root in the container
